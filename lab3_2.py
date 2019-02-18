e0eb8f18e18cd943b9e09fddc45e1e69351f925b
EntityFile <47143799+EntityFile@users.noreply.github.com>
2019-02-18 23:41:46 +0200

    Update lab3_2.py

--- lab3_2.py
+++ lab3_2.py
@@ -2,13 +2,50 @@ import twitter2
 import json
 
 
+def create_file(id_var):
+    twitter2.create_json_file(id_var)
+
 def open_and_read_json_file(file):
     f = open(file,encoding = 'utf-8')
     ff = json.load(f)
     return ff
 
+
 def main():
-    ff = open_and_read_json_file('info.json')
-    for el in ff['users']:
-        print(el)
+    id_var = input('Type your twitter id: ')
+    create_file(id_var)
+    f = open_and_read_json_file('info.json')
+    lst = []
+    ff = f
+    ex = 0
+    while True:
+        print('Elements: ',end = '')
+        space_ind = 0
+        for el in ff:
+            if space_ind == 1:
+                print(', ' + str(el),end = '')
+            else:
+                print(str(el),end = '')
+            space_ind = 1
+        if ex == 0:
+            choise = input('\nType: ')
+        else:
+            choise = int(input('\nType number from 1 to ' + str(len(ff)) + ': '))
+            ex = 0
+        lst.append(choise)
+        try:
+            if not type(choise) == int:
+                if type(ff[choise]) == dict:
+                    ff = ff[choise]
+                elif type(ff[choise]) == list:
+                    ff = ff[choise]
+                    ex = 1
+                else:
+                    print('Answer: ' + str(ff[choise]))
+                    print('History of selections: ' + str(lst))
+                    break
+            else:
+                ff = ff[choise-1]
+        except:
+            print('Wrong atribute')
 main()
